781ed9cbf03b8edcaa77e509437f4c4c4d767a0d
DioWang67 <105845893+DioWang67@users.noreply.github.com>
2026-04-01 15:33:48 +0800

    Replace Cassie with Unitree H1: full serial joints, PyBullet compatible

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,4 +1,5 @@
 from envs.ball_walker_env import BallWalkerEnv
 from envs.cassie_env import CassieEnv
+from envs.h1_env import H1Env
 
-__all__ = ["BallWalkerEnv", "CassieEnv"]
+__all__ = ["BallWalkerEnv", "CassieEnv", "H1Env"]
